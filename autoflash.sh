7f5f0c1a62067548fe81045acccee249edee1f17
Maciej Krüger <mkg20001@gmail.com>
2018-10-08 22:22:37 +0200

    Upgrade twrp

--- autoflash.sh
+++ autoflash.sh
@@ -140,7 +140,7 @@ latest_factory() {
 }
 
 latest_twrp() {
-  echo "$PRIVATE_MIRROR/twrp-3.2.2-0-$CODE_NAME.img" # echo "https://eu.dl.twrp.me/bullhead/twrp-3.2.2-0-bullhead.img" # from https://eu.dl.twrp.me/bullhead/
+  echo "$PRIVATE_MIRROR/twrp-3.2.3-0-$CODE_NAME.img" # echo "https://eu.dl.twrp.me/bullhead/twrp-3.2.2-0-bullhead.img" # from https://eu.dl.twrp.me/bullhead/
 }
 
 lastest_magisk() {
